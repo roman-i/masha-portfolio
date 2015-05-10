89e02414aab003c6485915bbabff15dc671fbeb7
Roman Inozemtsev <roman.inozemtsev@gmail.com>
2015-05-10 11:44:25 -0700

    updated texts for portfolio

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -4,6 +4,7 @@ angular
 .module('mashaPortfolioAngularApp').
 factory('portfolio', function() {
     var pl = 'http://placehold.it/500x500';
+    var bigPl = 'http://placehold.it/1000x600';
 
     var VIDEO = 'video',
         MOBILE = 'mobile',
@@ -39,7 +40,7 @@ factory('portfolio', function() {
         id: 'race-the-bunny',
         image: 'images/thumbs/bunny.png',
         groups: [WEB, MOBILE],
-        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+        description: 'This is my thesis project for the Academy of Art University. It is a map-based mobile application. It helps people who are running late to get to their destination on time. Basically, the  user plays a simple game: racing with a bunny, that shows a required speed to get on time, for a carrot, which represents the user’s destination.',
         attachments: [
             {
                 image: 'images/bunny/screens.png'
@@ -57,71 +58,208 @@ factory('portfolio', function() {
         title: 'chew well',
         id: 'chew-well',
         image: 'images/thumbs/chew.png',
-        groups: [MOBILE]
+        groups: [MOBILE],
+        description: 'Chew well is an application that helps to improve your health by slowly chewing food. You can set up a timer for each bite and get an alert when it is time for the next one.',
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var airHockey = {
         title: 'air hockey',
         id: 'air-hockey',
         image: 'images/thumbs/airhockey.png',
-        groups: [MOBILE]
+        description: 'Air hockey is a mobile version of a game for all air hockey lovers. I worked as a designer on this weekend project. The design is clean and simple in a flat style. Enjoy!',
+        groups: [MOBILE],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var foodVis = {
         title: 'food visualization',
         id: 'food-visualization',
         image: 'images/thumbs/food.png',
-        groups: [DATA_VIS]
+        description: 'This is a PNG version of a website where you can find out what kind of food I ate last fall. I collected the data over a couple months to see the pattern and then visualized. Click on an image to see more details.',
+        groups: [DATA_VIS],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var musicVis = {
         title: 'music visualization',
         id: 'music-visualization',
         image: 'images/thumbs/music.png',
-        groups: [DATA_VIS]
+        description: 'This data visualization shows my listening habits during winter 2014. I was collecting data about my music by using lastfm scrobbler, IFTTT.com and Echonext API. It is a website, so check it out here (please be patient while it loads...)',
+        groups: [DATA_VIS],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var hyperLoop = {
         title: 'hyper loop',
         id: 'hyper-loop',
         image: 'images/thumbs/hyper.png',
-        groups: [WEB]
+        description: 'It\'s a concept for a hyperloop website. The source of information is this blog (http://www.teslamotors.com/sites/default/files/blog_attachments/hyperloop_alpha3.pdf)',
+        groups: [WEB],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var beardMen = {
         title: '8-bit beard men',
         id: '8-bit-beard-men',
         image: 'images/thumbs/beards.png',
-        groups: [ILLUSTRATION]
+        description: 'Once I tried to create an illustration in 8-bit style I become obsessed. It was a fun project for me to create beard men in different clothing. You can find here superheroes and characters from daily life. Click here to see the instagram of this project.',
+        groups: [ILLUSTRATION],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
     var origami = {
         title: 'origami illustrations',
         id: 'origami-illustrations',
         image: 'images/thumbs/origami.png',
-        groups: [ILLUSTRATION]
+        description: 'Here is an instagram account for this origami project. I am trying to keep it updated and add a new illustration at least once a week.',
+        groups: [ILLUSTRATION],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
-    var mixBit = {
-        title: 'mix bit videos',
-        id: 'mix-bit-videos',
+    var videos = {
+        title: 'videos',
+        id: 'videos',
         image: pl,
-        groups: [VIDEO]
+        description: 'Here are some videos from my internship at mixbit.com and videos that I created for fun.',
+        groups: [VIDEO],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
     };
 
-    var fireMan = {
-        title: 'meet fireman video',
-        id: 'meet-fireman-video',
-        image: pl,
-        groups: [VIDEO]
-    };
+    // var fireMan = {
+    //     title: 'meet fireman video',
+    //     id: 'meet-fireman-video',
+    //     image: pl,
+    //     groups: [VIDEO],
+    //     attachments: [
+    //         {
+    //             image: bigPl
+    //         },
+    //         {
+    //             image: bigPl
+    //         },
+    //         {
+    //             image: bigPl
+    //         }
+    //     ]
+    // };
 
-    var ballet = {
-        title: 'ballet video',
-        id: 'ballet-video',
+    // var ballet = {
+    //     title: 'ballet video',
+    //     id: 'ballet-video',
+    //     image: pl,
+    //     groups: [VIDEO],
+    //     attachments: [
+    //         {
+    //             image: bigPl
+    //         },
+    //         {
+    //             image: bigPl
+    //         },
+    //         {
+    //             image: bigPl
+    //         }
+    //     ]
+    // };
+
+    var weather = {
+        title: 'Weather',
+        id: 'weather',
         image: pl,
-        groups: [VIDEO]
-    };
+        description: 'The problem is that sometimes when you see the numbers, you can not actually understand how you need to dress up. The idea of the responsive website is to show not only actual information but also a character that would wear some suggested clothing for that kind of weather (layer, rain coat, umbrella etc). The goal is to create a user-friendly interface and useful suggestions for the user.',
+        groups: [WEB],
+        attachments: [
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            },
+            {
+                image: bigPl
+            }
+        ]
+    }
 
     var works = [
         raceTheBunny, 
@@ -131,10 +269,11 @@ factory('portfolio', function() {
         musicVis,
         beardMen,
         origami,
-        mixBit,
+        videos,
         hyperLoop,
-        fireMan,
-        ballet
+        // fireMan,
+        // ballet,
+        weather
     ];
 
     function findPos(id) {
